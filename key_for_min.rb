9ea4896ce3fa3ebea5564aaec259f27ff9446d12
Sara Nolen <nolensara1@gmail.com>
2021-01-09 13:55:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.collect{|key, value| key}
+return nil if name-hash.size ==0
 end
